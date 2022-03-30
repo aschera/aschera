b22eb35b33545f23c62749e665250e11c70a7b36
Christina Bögh <aschera@gmail.com>
2022-03-30 09:57:22 +0200

    fixed some parts of the mobile view

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent {
 
-  constructor() { }
+  htmlStr1: string = 'I’m an <b>Front end developer</b> who’s been creating experiences for the web for some years.';
 
-  ngOnInit(): void {
-  }
+  htmlStr2: string = 'I’m currently studying for a <br> <b>B.A. in Computer Science</b> <br> at the <a class="strike yellow-link" href=" https://www.uopeople.edu/" target="_blank">University of the People.</a> <br> I’m now in a my last year. <br> You can see a chart of completed courses <a class="strike yellow-link" href="../../assets/images/courses.png" target="_blank">here.</a>';
+
+  htmlStr3: string = 'I love learning new things. I am enjopying courses on Pluralsight, Udemy, EdX and Coursera and more.';
 
 }
--- src/app/design/design.component.ts
+++ src/app/design/design.component.ts
@@ -1,42 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-@Component({
-  selector: 'app-design',
-  templateUrl: './design.component.html',
-  styleUrls: ['./design.component.scss']
-})
+@Component({selector: 'app-design', templateUrl: './design.component.html', styleUrls: ['./design.component.scss']})
 
 export class DesignComponent implements OnInit {
 
-  list: Article[] = [
-    {
-      title: 'Text Spacing§ WCAG 2.1 Success Criterion 1.4.12 (Level AA)',
-      link: 'https://medium.com/@aschera/text-spacing-620220e003bc',
-      id: 1,
-      description: "This web accessibility criterion is really very important. It has four main parts. Line height, spacing after paragraphs, letter spacing and word spacing. It's about the space in and around sections of text.",
-    },
-    {
-      title: 'R — Subsetting',
-      link: 'https://medium.com/@aschera/r-subsetting-1d273131e642',
-      id: 2,
-      description: "In the functional language R, one can subset data in various ways. Subsetting is one of the fundamental operations and this article outlines the basics.",
-    },
-  ];
+    list : Article[] = [
+        {
+            title: 'Text Spacing§ WCAG 2.1 Success Criterion 1.4.12 (Level AA)',
+            link: 'https://medium.com/@aschera/text-spacing-620220e003bc',
+            id: 1,
+            description: "This web accessibility criterion is really very important. It has four main parts. Line height, spacing after paragraphs, letter spacing and word spacing. It's about the space in and around sections of text."
+        }, {
+            title: 'R — Subsetting',
+            link: 'https://medium.com/@aschera/r-subsetting-1d273131e642',
+            id: 2,
+            description: "In the functional language R, one can subset data in various ways. Subsetting is one of the fundamental operations and this article outlines the basics."
+        },
+    ];
 
+    title : String = "Recent Articles";
+    intro : String = "I write a lot of essays and articles during my studies, and also as a hobby for the things that I want to learn. Learning is my biggest passion. Writing keeps me remembering and structuring my thoughts.";
+    medium : String = 'https://medium.com/@aschera';
 
-  title: String = "Recent Articles";
+    constructor() {}
+    ngOnInit(): void {}
 
-  intro: String = "I write a lot of essays and articles during my studies, and also as a hobby for the things that I want to learn. Learning is my biggest passion. Writing keeps me remembering and structuring my thoughts.";
-
-  medium: String= 'https://medium.com/@aschera';
-
-  constructor() {}
-  ngOnInit(): void {}
 }
 
 interface Article {
-  title: string,
-  link: string,
-  id: number,
-  description: string,
+    title: string,
+    link: string,
+    id: number,
+    description: string
 }
