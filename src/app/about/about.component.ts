d3b95f85dfa0f55f8330612f4402574770e96186
Ascher <aschera@gmail.com>
2023-01-20 08:53:02 +0100

    reshaping the website for a new look

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,10 +7,12 @@ import { Component} from '@angular/core';
 })
 export class AboutComponent {
 
-  htmlStr1: string = 'I’m an <b>Front end developer</b> who’s been creating experiences for the web for some years.';
+  htmlStr1: string = 'I’m an <b>data science student</b> who has been creating engaging web experiences for several years as a front-end developer. Now, I am focused on utilizing data to generate valuable insights and drive business decisions.';
+  
+  htmlStr2: string = 'I’m currently studying for a <b>B.A. in Computer Science</b> <br> at the <a class="strike yellow-link" href=" https://www.uopeople.edu/" target="_blank">University of the People</a> , and am in my last year of study. <br> As a student, I’ve completed a variety of computer science courses, which you can view in my portfolio <a class="strike yellow-link" href="../../assets/images/courses.png" target="_blank">here.</a>';
 
-  htmlStr2: string = 'I’m currently studying for a <br> <b>B.A. in Computer Science</b> <br> at the <a class="strike yellow-link" href=" https://www.uopeople.edu/" target="_blank">University of the People.</a> <br> I’m now in a my last year. <br> You can see a chart of completed courses <a class="strike yellow-link" href="../../assets/images/courses.png" target="_blank">here.</a>';
-
-  htmlStr3: string = 'I love learning new things. I am enjopying courses on Pluralsight, Udemy, EdX and Coursera and more.';
+  htmlStr4: string = 'In addition, I am pursuing a <b>vocational degree in Data science</b>  at <a class="strike yellow-link" href="" target="_blank">EC Utbildning </a> where I have completed courses in business analytics, R, statistics and SQL. <br>';
+  
+  htmlStr3: string = 'In my free time, I enjoy continuing my education through online platforms like Pluralsight, Udemy, EdX, and Coursera. ';
 
 }
